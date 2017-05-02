eaa47ace0ec7d0c6d5b47cba83bcccc5db479628
mebao <ke.v.pham@vntrip.vn>
2017-05-02 07:49:03 +0700

    The First

---
+++ thank.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--[if IE 7 ]><body class="ie ie7"><![endif]-->
+<!--[if IE 8 ]><body class="ie ie8"><![endif]-->
+<!--[if IE 9 ]><body class="ie ie9"><![endif]-->
+<html class='no-js' lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, maximum-scale=1.0">
+        <title>Tital gel Nga - Yêu Lành Mạnh</title>
+        <meta content="Titan gel Nga, Titan gel" name="keywords">
+        <meta content="Titan gel Nga, Nhà phân phối Yêu Lành Mạnh" name="description">
+        <link rel="shortcut icon" href="img/favicon.png" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&subset=vietnamese" rel="stylesheet">
+        <link rel="shortcut icon" href="media/images/favicon.png" />
+        <link rel="stylesheet" href="thank/css/bootstrap.css">
+        <link rel="stylesheet" href="thank/css/font-awesome.css">
+        <link rel="stylesheet" href="thank/css/main.css">
+        <link rel="stylesheet" href="thank/css/responsive.css">
+    </head>
+    <body>
+        <section class="body-sign">
+            <div class="center-sign">
+                <div class="tkbn">
+                    <a href="http://yeulanhmanh.com/" class="">
+                        <img src="thank/images/thanks.png" style="max-width:350px;">
+                    </a>
+                </div>
+                <div class="">
+                    <div class="panel-body" style="text-align:center">
+                        <p class="tkf">Cảm ơn bạn đã đặt mua</p>
+                        <p class="tkdes">Chúng tôi sẽ liên hệ với bạn trong thời gian sớm nhất.</p>
+                        <p class="tkdes">Bản quyền thuộc về <a href="http://yeulanhmanh.com/" style="color: red;">"Yêu Lành Mạnh"</a></p>
+                        <div class="row">
+                            <div class="col-sm-12 btn-end">
+                                <a href="http://banstud100.net/" class="btn-lg-x">Trở về trang chủ</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
